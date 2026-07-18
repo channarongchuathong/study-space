4b9c4bb97cfe90f361c05ab3bc5f733aac1c4d40
channarongchuathong <channarongchuathong@gmail.com>
2026-07-18 19:08:23 +0700

    home layout

--- src/pages/้Home.tsx
+++ src/pages/้Home.tsx
@@ -1,8 +1,11 @@
+import Course from "../components/Course";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import Search from "../components/Search";
 
 function Home() {
   return (
-    <div>
+    <div className="bg-background">
       <Navbar />
       <section className="bg-background bg-[url('/images/hero.png')] bg-cover bg-right">
         <div className="container mx-auto">
@@ -24,6 +27,56 @@ function Home() {
           </div>
         </div>
       </section>
+      <Search></Search>
+      <Course></Course>
+      <section className="mb-10">
+        <div className="container mx-auto">
+          <div className="flex justify-between text-text border border-border rounded-xl p-10 divide-x divide-border bg-surface">
+
+            <div className="flex justify-between px-8 flex-1 gap-3 items-center">
+              <div className="w-16 h-16">
+                <img src="/images/icon/icon-1.png" alt="icon" className="h-full w-full object-contain"/>
+              </div>
+              <div>
+                <h1 className="font-semibold text-lg">เรียนได้ทุกที่ ทุกเวลา</h1>
+                <p className="text-muted">เรียนผ่านทุกอุปกรณ์<br/>ได้ทุกที่ที่คุณต้องการ</p>
+              </div>
+            </div>
+
+            <div className="flex justify-between px-8 flex-1 gap-3 items-center">
+              <div className="w-16 h-16">
+                <img src="/images/icon/icon-2.png" alt="icon" className="h-full w-full object-contain"/>
+              </div>
+              <div>
+                <h1 className="font-semibold text-lg">ติดตามความก้าวหน้า</h1>
+                <p className="text-muted">ระบบติดตามการเรียนรู้<br/>ช่วยให้คุณพัฒนาต่อเนื่อง</p>
+              </div>
+            </div>
+
+           <div className="flex justify-between px-8 flex-1 gap-3 items-center">
+              <div className="w-16 h-16">
+                <img src="/images/icon/icon-3.png" alt="icon" className="h-full w-full object-contain"/>
+              </div>
+              <div>
+                <h1 className="font-semibold text-lg">เรียนเป็นขั้น เป็นตอน</h1>
+                <p className="text-muted">เนื้อหาจัดเป็นบทเรียน<br/>เข้าใจง่ายและนำไปใช้ได้จริง</p>
+              </div>
+            </div>
+
+           <div className="flex justify-between px-8 flex-1 gap-3 items-center">
+              <div className="w-16 h-16">
+                <img src="/images/icon/icon-4.png" alt="icon" className="h-full w-full object-contain"/>
+              </div>
+              <div>
+                <h1 className="font-semibold text-lg">เรียนรู้จากผู้เชี่ยวชาญ</h1>
+                <p className="text-muted">คอร์สคุณภาพจากผู้สอนตัวจริง<br/>ในแต่ละสายงาน</p>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </section>
+      <Footer></Footer>
     </div>
 
 
